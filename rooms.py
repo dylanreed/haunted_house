7612527e5017d5631bbc73fc60806ef529371a21
Dylan Reed <dylan@dylanreed.comgit config --global github.user awesomeguygit config --global user.email dylan@dylanreed.co>
2012-05-21 15:18:40 -0600

    first commit

---
+++ rooms.py
@@ -0,0 +1,134 @@
+#use this format when adding a new room
+#genericroom = """This is a generic room"""
+
+#intro description for your game
+intro = """Welcome to the Haunted House.
+This house was inspired by:
+Betrayal on the House on the Hill
+For ease of playing you can abbreviate
+North, South, East and West as N, S, E and W
+Other words will need to be typed in fully
+in lower case.
+Enjoy!"""
+
+#the rest are all rooms on the main floor of the house
+outsidedesc = """It is a stormy night, lightning flashes and thunder 
+crashes around Old Man Farley's mansion. The mansion is huge. 
+From the outside you see a few lights on and think you see somehthing
+moving in windows. Do you enter?"""
+
+entrancehall = """You find yourself in a surprisingly well lit entrance hall. 
+There are coat racks to hang your coat on, in fact all that 
+is missing is a butler. Further down the hall you see through
+a foyer a set of stairs. There are doors to the west and east
+of you as well as the foyer to the north. What door do you 
+want to go through?"""
+
+creekyhall="""Through this door you find yourself in a hallway, 
+every step causing the floorboards to creek. You pause after 
+every step but don't hear anything else moving in the house. 
+At the end of the hall is a dooway. Do you go through that 
+door(w) or head back the way you came(e)?"""
+
+ballroom="""This beautifully decorated ballroom is dusty and 
+looks unused. As you reach the center of the room you hear a 
+hidious shriek from somewhere else in the house. There is a 
+door to the south and the door you entered from(e). Which 
+door do you want to go through?"""
+
+coalchute= """You enter this room and the first thing you notice 
+is that it is pitch black. Shortly thereafter you notice the 
+strange lack of floor as you tumble down. Landing in a heep 
+you feel for injuries and stand up.
+"""
+
+foyerdesc="""Foyer exits to North and West"""
+
+grandstairs="""Stairs to second floor. Take them?"""
+
+graveyard="""So many graves, can only go east from here"""
+
+charredroom="""Looks like a fire went through here. Doors every direction"""
+
+diningroom="""Where the eating happens. Back north or keep going east"""
+
+gameroom="""No xbox here. west, north or east?"""
+
+conservatorydesc="""What is a conservatory? Can only go back west from here"""
+
+gardendesc="""Look at all the plants. Can go west, north or south."""
+
+abandonedroom="""Looks like no one has been here for years. North, south or west?"""
+
+kitchendesc="""Where is the pie? SOuth or west?"""
+
+patiodesc="""Time to relax. East or South?"""
+
+dustyhall="""AHHHH CHOOOO!!!!! So much dust. All directions availible."""
+
+#these are all rooms on the top floor of the house
+masterbed ="""master_bed S"""
+
+gallerydesc = """gallery N,S"""
+
+librarydesc= """library n,e"""
+
+chapeldesc= """chapel S"""
+
+towerdesc="""tower n,s"""
+
+bloodyroom="bloody_room n,e,w"""
+
+vault="""vault S"""
+
+researchlab="""research_lab n,s"""
+
+landingdesc="""Landing, n,e,w,s"""
+
+atticdesc ="""attic s"""
+
+bedroomdesc="""bedroom n,s"""
+
+junkroom= """junk_room n,e,w"""
+
+collapsedroom="""collapsed_room w,n"""
+
+balconydesc="""balcony s"""
+
+#these are all rooms in the basement of the house
+
+basementlanding="""You are in a room full of coal dust. Looking 
+up you see that you fell down the coal shoot and there is no way 
+up. There are doors on all side of you. Which door do you enter?"""
+
+larderdesc="""Food goes here. E + W"""
+
+catacombsdesc="""catacombs e+w"""
+
+cryptdesc="""tales from the crypt. W only"""
+
+gymdesc="""Gym E + S """
+
+chasmdesc="""A deep hole in the ground be careful N +S"""
+
+organroom="""play that organ. S and w"""
+
+storeroom="""Store stuff here. East only"""
+
+furnaceroom="""It sure is hot in here. N, E, W"""
+
+operatingroom="""Surgery please. N, W"""
+
+wineceller="""Casks of wine everywhere. w,e"""
+
+serventsquarters="""where the butler sleeps. all directions"""
+
+mysticelevator="""where it goes no one knows. just N"""
+
+pentagramchamber="""where you go to sacrifice what you love. e only"""
+
+statuaryhall="""A hall full of statues. N and S only"""
+
+undergroundlake="""What is all this water doing here? E + S"""
+
+stairstofoyer="""These stairs seem to go up...to the foyer."""
