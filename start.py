3da807de21772edb1f5855761325336a1ffa36ea
Dylan Reed <dylan@dylanreed.comgit config --global github.user awesomeguygit config --global user.email dylan@dylanreed.co>
2012-05-22 14:16:44 -0600

    This is where the game starts now

---
+++ start.py
@@ -0,0 +1,12 @@
+import random
+from sys import exit
+from rooms import *
+from badthings import *
+from goodthings import *
+import haunted_house
+import death
+
+#Game Start
+def start():
+	haunted_house.intro()
+
